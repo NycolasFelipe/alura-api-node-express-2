01cfc69452dee06525c8e27494ad1613c4e376dd
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2023-06-14 21:17:09 -0300

    Refatorando o manipulador de erros

---
+++ src/errors/ErroValidacao.js
@@ -0,0 +1,10 @@
+import RequisicaoIncorreta from "./RequisicaoIncorreta.js";
+
+class ErroValidacao extends RequisicaoIncorreta {
+  constructor(err) {
+    const mensagensErro = Object.values(err.errors).map(erro => erro.message).join("; ");
+    super(`Os seguintes erros foram encontrados: ${mensagensErro}`);
+  }
+}
+
+export default ErroValidacao;
